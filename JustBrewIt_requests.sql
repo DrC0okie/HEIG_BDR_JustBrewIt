faf44fb0f69d875b516c6b850f0798b1174a773c
ThomasGermano <thomas.germano@heig-vd.ch>
2022-12-11 21:00:18 +0100

    added first view and function request

--- JustBrewIt_requests.sql
+++ JustBrewIt_requests.sql
@@ -1,1 +1,112 @@
 SET search_path TO justBrewIt;
+
+-- obtenir les ingrédients d'une recette donnée
+
+DROP FUNCTION getIngredientsFromRecipes;
+
+CREATE OR REPLACE FUNCTION getIngredientsFromRecipes
+(
+    recipeId integer
+)
+RETURNS TABLE(nom varchar, quantite real, unite varchar)
+language plpgsql
+AS
+$$
+    BEGIN
+    RETURN QUERY SELECT name, quantity, quantity_unit
+        FROM ingredientsfromrecipes
+    WHERE recipe_number = recipeId;
+    END;
+$$;
+
+-- obtenir les ingrédients d'une étape donnée
+
+DROP FUNCTION getIngredientsFromStep;
+
+CREATE OR REPLACE FUNCTION getIngredientsFromStep
+(
+    recipeId integer,
+    stepId integer
+)
+RETURNS TABLE (nom varchar, quantite real, unite varchar)
+language plpgsql
+AS
+$$
+    BEGIN
+    RETURN QUERY SELECT name, quantity, quantity_unit
+        FROM ingredientsfromrecipes
+    WHERE recipe_number = recipeId
+    AND step_number = stepId;
+    END;
+$$;
+
+-- obtenir la bière issue d'une recette
+
+DROP FUNCTION getBeerFromRecipe;
+
+CREATE OR REPLACE FUNCTION getBeerFromRecipe
+(
+    recipeId integer
+)
+RETURNS TABLE (nom varchar, couleur integer, alcool real, amertume integer)
+language plpgsql
+AS
+$$
+    BEGIN
+    RETURN QUERY SELECT name, color, alcohol, bitterness
+        FROM beersfromrecipes
+    WHERE recipe_number = recipeId;
+    END;
+$$;
+
+-- obtenir le panier actuel d'un utilisateur
+
+DROP FUNCTION getCartFromCustomer;
+
+CREATE OR REPLACE FUNCTION getCartFromCustomer
+(
+    customerId integer
+)
+RETURNS TABLE (nom varchar, quantite real, unite varchar)
+language plpgsql
+AS
+$$
+    BEGIN
+    RETURN QUERY SELECT name, quantity, quantity_unit
+        FROM ordersfromcustomers
+    WHERE customer_id = customerId
+    AND ordered = false;
+    END;
+$$;
+
+
+-- obtenir les commandes d'un utilisateur
+DROP FUNCTION getOrdersFromCustomer;
+
+CREATE OR REPLACE FUNCTION getOrdersFromCustomer
+(
+    customerId integer
+)
+RETURNS TABLE (numero integer, nom varchar, quantite real, unite varchar)
+language plpgsql
+AS
+$$
+    BEGIN
+    RETURN QUERY SELECT order_number, name, quantity, quantity_unit
+        FROM ordersfromcustomers
+    WHERE customer_id = customerId
+    AND ordered = true;
+    END;
+$$;
+
+-- obtenir prochaine étape
+
+-- obtenir les informations concernant une recette donnée
+
+-- obtenir les informations concernant une étape donnée
+
+-- calculer le temps restant à une étape
+
+-- ajouter une recette
+
+-- modifier une recette
--- JustBrewIt_views.sql
+++ JustBrewIt_views.sql
@@ -1,1 +1,49 @@
 SET search_path TO justBrewIt;
+
+DROP VIEW ingredientsFromRecipes;
+
+CREATE OR REPLACE VIEW ingredientsFromRecipes AS
+    SELECT r.recipe_number, bs.step_number, i.name, iu.quantity, i.quantity_unit
+    FROM recipe r
+        INNER JOIN brewing_step bs
+            ON r.recipe_number = bs.recipe_number_fk
+        INNER JOIN ingredient_usage iu
+            ON bs.step_number = iu.step_number_fk
+                   AND bs.recipe_number_fk = iu.recipe_number_fk
+        INNER JOIN ingredient i
+            ON i.ingredient_id = iu.ingredient_id_fk;
+
+DROP VIEW recipesFromCustomers;
+
+CREATE OR REPLACE VIEW recipesFromCustomers AS
+    SELECT customer_id, recipe_number
+    FROM customer
+        INNER JOIN recipe r ON customer.customer_id = r.creator_id_fk;
+
+DROP VIEW ordersFromCustomers;
+
+CREATE OR REPLACE VIEW ordersFromCustomers AS
+    SELECT customer_id, o.order_number
+    FROM customer
+        INNER JOIN "order" o
+            ON customer.customer_id = o.customerid_fk;
+
+DROP VIEW beersFromRecipes;
+
+CREATE OR REPLACE VIEW beersFromRecipes AS
+    SELECT recipe_number, b.name, b.color, b.alcohol, b.bitterness
+        FROM recipe
+            INNER JOIN beer b
+                ON b.beer_id = recipe.beer_id_fk;
+
+DROP VIEW ordersFromCustomers;
+
+CREATE OR REPLACE VIEW ordersFromCustomers AS
+    SELECT customer_id, order_number, ordered, i.name, iq.quantity, i.quantity_unit
+    FROM "order"
+        INNER JOIN customer c
+            ON c.customer_id = "order".customerid_fk
+        INNER JOIN ingredient_quantity iq
+            ON "order".order_number = iq.order_number_fk
+        INNER JOIN ingredient i
+            ON i.ingredient_id = iq.ingredient_id_fk;
