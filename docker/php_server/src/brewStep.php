07ca05081d85d5da0fe9ac78fb97d177f5c0b3db
DrC0okie <timothee.vanhove@heig-vd.ch>
2023-01-22 12:35:25 +0100

    Added the brew_step page

---
+++ docker/php_server/src/brewStep.php
@@ -0,0 +1,43 @@
+<?php include './header.php';?>
+
+		
+<main class="p-6">
+
+	<?php
+		if (isset($_GET['recipe_number'])) {
+			$recipeNumber = $_GET['recipe_number'];
+			$stepNumber = $_GET['step_number'];
+			$stepCount = $_GET['step_count'];
+			$nextStep = $stepNumber + 1;
+			$query = $db->prepare("SELECT * FROM getStepInfo(:recipeNumber, :stepNumber)");
+			$query->bindParam(':recipeNumber', $recipeNumber);
+			$query->bindParam(':stepNumber', $stepNumber);
+			$query->execute();
+			$step = $query->fetch();
+	?>
+	<div class="card">
+		<h2 class="text-lg font-medium">Etape n°<?= $stepNumber ?>: <?= $step['step_name'] ?></a></h2>
+		<div class="stepCard">
+			<h3 class="text-lg font-medium">Durée: <?= $step['duration']?> minutes,  Catégorie: <?= $step['category'] ?></a></h3>
+			<p class="card-text">Description: <?= $step['description'] ?></p>
+		</div>
+	</div>
+	
+
+	<button 
+    <?php if ($stepCount == $stepNumber): ?>
+		style="display:none" disabled
+    <?php else: ?>
+        class="bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600"
+    <?php endif; ?>
+    onclick="window.location.href='./brewStep.php?recipe_number=<?= $recipeNumber ?>&step_count=<?= $stepCount ?>&step_number=<?= $nextStep ?>'">
+        Prochaine étape
+	</button>
+
+
+	<?php } else {
+			header("Location: login.php");
+		}
+	?>
+</main>
+<?php include './footer.php';?>
